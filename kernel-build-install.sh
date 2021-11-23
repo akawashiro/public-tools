ac8015c929fbac2419d8a1efba375d66af8fc683
Akira Kawata <akirakawata1@gmail.com>
2021-11-23 09:50:28 +0900

    Delete KEY

--- kernel-build-install.sh
+++ kernel-build-install.sh
@@ -6,7 +6,9 @@ BRANCH_NAME=fix-load_addr-v2
 cd ${LINUX_DIR}
 git fetch --all
 git checkout origin/${BRANCH_NAME}
-cp /boot/config-$(uname -r) .config
+cp /boot/config-$(uname -r) ${LINUX_DIR}/.config
+cat ${LINUX_DIR}/.config | sed -e "s/CONFIG_SYSTEM_TRUSTED_KEYS=".*"/CONFIG_SYSTEM_TRUSTED_KEYS=\"\"/g" > ${LINUX_DIR}/.config.bak
+cp ${LINUX_DIR}/.config.bak ${LINUX_DIR}/.config
 LOCALVERSION=-dev-${BRANCH_NAME} make CC="ccache gcc" -j4
 sudo make modules_install
 sudo make install
