ebfe19840f988001f5febe38f6e24a389484c443
Akira Kawata <akirakawata1@gmail.com>
2021-11-26 12:57:29 +0900

    Update

--- kernel-build-install.sh
+++ kernel-build-install.sh
@@ -5,7 +5,7 @@ BRANCH_NAME=fix-load_addr-v2
 
 if [[ ! -d "${LINUX_DIR}" ]]
 then
-    git clone git@github.com:akawashiro/linux.git "${LINUX_DIR}"
+    git clone https://github.com/akawashiro/linux.git "${LINUX_DIR}"
 fi
 
 cd ${LINUX_DIR}
@@ -15,7 +15,7 @@ cp /boot/config-$(uname -r) ${LINUX_DIR}/.config
 cat ${LINUX_DIR}/.config | sed -e "s/CONFIG_SYSTEM_TRUSTED_KEYS=".*"/CONFIG_SYSTEM_TRUSTED_KEYS=\"\"/g" | sed -e "s/CONFIG_SYSTEM_REVOCATION_KEYS=".*"/CONFIG_SYSTEM_REVOCATION_KEYS=\"\"/g" > ${LINUX_DIR}/.config.bak
 cp ${LINUX_DIR}/.config.bak ${LINUX_DIR}/.config
 make localmodconfig
-LOCALVERSION=-dev-${BRANCH_NAME} make CC="ccache gcc" -j4
+LOCALVERSION=-dev-${BRANCH_NAME} make CC="ccache gcc" -j
 sudo make modules_install
 sudo make install
 
