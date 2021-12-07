7f4bcacc5e04d79eba1fc10a876910ca1d9df32e
Akira Kawata <akirakawata1@gmail.com>
2021-12-07 22:39:08 +0900

    Install v4

--- kernel-build-install.sh
+++ kernel-build-install.sh
@@ -4,7 +4,7 @@ sudo apt-get -y build-dep linux linux-image-$(uname -r)
 sudo apt-get -y install libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf git ccache dwarves
 
 LINUX_DIR=${HOME}/linux
-BRANCH_NAME=fix-load_addr-v2
+BRANCH_NAME=fix-load_addr-v4
 
 if [[ ! -d "${LINUX_DIR}" ]]
 then
@@ -18,7 +18,7 @@ cp /boot/config-$(uname -r) ${LINUX_DIR}/.config
 cat ${LINUX_DIR}/.config | sed -e "s/CONFIG_SYSTEM_TRUSTED_KEYS=".*"/CONFIG_SYSTEM_TRUSTED_KEYS=\"\"/g" | sed -e "s/CONFIG_SYSTEM_REVOCATION_KEYS=".*"/CONFIG_SYSTEM_REVOCATION_KEYS=\"\"/g" > ${LINUX_DIR}/.config.bak
 cp ${LINUX_DIR}/.config.bak ${LINUX_DIR}/.config
 make localmodconfig
-LOCALVERSION=-dev-${BRANCH_NAME} make CC="ccache gcc" -j
+LOCALVERSION=-dev-${BRANCH_NAME} make CC="ccache gcc" -j 3
 sudo make modules_install
 sudo make install
 
