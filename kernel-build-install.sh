c3f6da2fc4d22ee28821f22cf983deb86119dd0c
Akira Kawata <a.kawashiro@gmail.com>
2021-12-09 21:18:54 +0900

    Delete old kernels

--- kernel-build-install.sh
+++ kernel-build-install.sh
@@ -9,6 +9,12 @@ BRANCH_NAME=fix-load_addr-patch-v4
 USE_CPUS=$(nproc --all)
 USE_CPUS=$((USE_CPUS-2))
 
+pushd /boot
+ls -1 | grep ${BRANCH_NAME} | xargs -I sudo rm {}
+popd
+sudo update-grub
+exit 0
+
 if [[ ! -d "${LINUX_DIR}" ]]
 then
     git clone https://github.com/akawashiro/linux.git "${LINUX_DIR}"
@@ -22,7 +28,8 @@ make clean
 # make CC="ccache gcc" O=${BUILD_DIR} olddefconfig 
 
 # cd ${BUILD_DIR}
-cp /boot/config-$(uname -r) .config
+# cp /boot/config-$(uname -r) .config
+cp /boot/config-5.11.0-40-generic .config
 cat .config | \
     sed -e "s/CONFIG_SYSTEM_TRUSTED_KEYS=".*"/CONFIG_SYSTEM_TRUSTED_KEYS=\"\"/g" | \
     sed -e "s/CONFIG_SYSTEM_REVOCATION_KEYS=".*"/CONFIG_SYSTEM_REVOCATION_KEYS=\"\"/g" \
