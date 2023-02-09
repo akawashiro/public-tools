84a1fa734f62e4b1848a5e8cc0cd582b4f928844
Akira Kawata <akirakawata1@gmail.com>
2023-02-09 10:27:42 +0900

    Show dstat more

--- check-heavy.sh
+++ check-heavy.sh
@@ -5,9 +5,14 @@ tmux new -s check-heavy -d
 tmux send-keys -t check-heavy 'tmux split-window -h -t 0' C-m
 tmux send-keys -t check-heavy 'tmux split-window -v -t 0' C-m
 tmux send-keys -t check-heavy 'tmux split-window -v -t 2' C-m
+tmux send-keys -t check-heavy 'tmux split-window -v -t 2' C-m
+
+tmux send-keys -t check-heavy 'tmux select-layout tiled' C-m
+
 tmux send-keys -t check-heavy 'tmux respawn-pane -k -t 1 "htop"' C-m
 tmux send-keys -t check-heavy 'tmux respawn-pane -k -t 2 "sudo dmesg -wH"' C-m
-tmux send-keys -t check-heavy 'tmux respawn-pane -k -t 3 "dstat"' C-m
+tmux send-keys -t check-heavy 'tmux respawn-pane -k -t 3 "sudo journalctl -f"' C-m
+tmux send-keys -t check-heavy 'tmux respawn-pane -k -t 4 "dstat -cdngyip --top-io --top-bio --top-mem --top-cpu"' C-m
 tmux send-keys -t check-heavy 'sudo iftop' C-m
 
 tmux attach -t check-heavy
