a6ff83659649c78def8c6f545dcdebd8a893da1a
Akira Kawata <akirakawata1@gmail.com>
2024-07-05 23:12:44 +0900

    Rename script

--- build-pytorch.sh
+++ build-pytorch.sh
@@ -1,27 +1,41 @@
 #! /bin/bash -eux
 
-# git clone --branch v1.13.0 git@github.com:pytorch/pytorch.git pytorch
-
 SOURCE_DIR=$(ghq list -p | grep pytorch | head -n 1)
+
 BUILD_DIR=${HOME}/tmp/pytorch-build
 INSTALL_DIR=${HOME}/tmp/pytorch-install
 mkdir -p ${BUILD_DIR}
 mkdir -p ${INSTALL_DIR}
 
 pushd ${SOURCE_DIR}
-git submodule update --init --recursive
-popd
+rm -rf build
+ln -sf ${BUILD_DIR} build
 
-cmake -S ${SOURCE_DIR} -B ${BUILD_DIR} -G Ninja \
-    -DBUILD_SHARED_LIBS:BOOL=ON \
-    -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo \
-    -DPYTHON_EXECUTABLE:PATH=`which python3` \
-    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-    -DUSE_CUDA=OFF \
-    -DUSE_CUDNN=OFF \
-    -DUSE_ROCM=OFF \
-    -DUSE_NCCL=OFF \
-    -DUSE_NNPACK=OFF \
-    -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
-    -DCMAKE_INSTALL_PREFIX:PATH=${INSTALL_DIR}
-cmake --build ${BUILD_DIR} -- -j12 install
+export CMAKE_EXPORT_COMPILE_COMMANDS=1
+export DEBUG=1
+export MAX_JOBS=16
+export USE_CUDA=0
+export USE_MKLDNN=0
+export USE_NNPACK=0
+export USE_QNNPACK=0
+export USE_DISTRIBUTED=0
+export USE_NNPACK=0
+export USE_QNNPACK=0
+export USE_DISTRIBUTED=0
+export USE_TENSORPIPE=0
+export USE_GLOO=0
+export USE_MPI=0
+export USE_SYSTEM_NCCL=0
+export BUILD_CAFFE2_OPS=0
+export BUILD_CAFFE2=0
+export USE_IBVERBS=0
+export USE_OPENCV=0
+export USE_OPENMP=0
+export USE_FFMPEG=0
+export USE_FLASH_ATTENTION=0
+export USE_MEM_EFF_ATTENTION=0
+export USE_LEVELDB=0
+export _GLIBCXX_USE_CXX11_ABI=1
+
+python3 setup.py build --build-base ${BUILD_DIR} --debug
+python3 setup.py install --prefix ${INSTALL_DIR}
