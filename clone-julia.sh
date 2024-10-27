3f8d36c25ff736b9f1b7cc62de3292ffc51e8dbd
Akira Kawata <akirakawata1@gmail.com>
2024-10-27 22:21:26 +0900

    Use ghq

--- clone-julia.sh
+++ clone-julia.sh
@@ -1,11 +1,6 @@
 #!/bin/bash -eux
 
-if [ $# -ne 1 ]; then
-    echo "You must specify the target directory."
-    exit 1
-fi
-
-git clone git@github.com:akawashiro/julia.git $1
-cd $1
+ghq get git@github.com:akawashiro/julia.git
+cd $(ghq root)/github.com/akawashiro/julia
 git remote add upstream git@github.com:JuliaLang/julia.git
 git remote set-url upstream --push no-push
