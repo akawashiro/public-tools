e441a6d8c3c3cc7b6c98f78453dc9f56de9d2314
Akira Kawata <akirakawata1@gmail.com>
2025-10-11 22:10:19 +0900

    Fix script name

--- setup-with-root.sh
+++ setup-with-root.sh
@@ -9,4 +9,4 @@ sudo apt update
 sudo apt build-dep -y linux
 sudo apt install -y libtool libtool-bin gnustep-gui-runtime automake lua-system lua-term lua-dkjson lua-mediator lua-filesystem curl git zsh tmux openssh-server curl python3-dev python3-pip ncurses-dev lua5.2 luajit ruby-dev libperl-dev software-properties-common build-essential silversearcher-ag htop pwgen m4 cmake tig xclip gettext neovim libclang-dev fakeroot libncurses-dev iftop git-email neomutt golang-go locales-all pass
 
-$HOME/public-tools/setup-non-root.sh
+$HOME/public-tools/setup-without-root.sh
