7dafc6550df6232235d1394a2649e3196ee8c753
Akira Kawata <akirakawata1@gmail.com>
2025-10-18 23:01:50 +0900

    Install dstat

--- setup-with-root.sh
+++ setup-with-root.sh
@@ -7,6 +7,6 @@ sudo sed -i 's/^Types: deb$/Types: deb deb-src/' /etc/apt/sources.list.d/ubuntu.
 sudo add-apt-repository -y ppa:neovim-ppa/stable
 sudo apt update
 sudo apt build-dep -y linux
-sudo apt install -y libtool libtool-bin gnustep-gui-runtime automake lua-system lua-term lua-dkjson lua-mediator lua-filesystem curl git zsh tmux openssh-server curl python3-dev python3-pip ncurses-dev lua5.2 luajit ruby-dev libperl-dev software-properties-common build-essential silversearcher-ag htop pwgen m4 cmake tig xclip gettext neovim libclang-dev fakeroot libncurses-dev iftop git-email neomutt golang-go locales-all pass
+sudo apt install -y libtool libtool-bin gnustep-gui-runtime automake lua-system lua-term lua-dkjson lua-mediator lua-filesystem curl git zsh tmux openssh-server curl python3-dev python3-pip ncurses-dev lua5.2 luajit ruby-dev libperl-dev software-properties-common build-essential silversearcher-ag htop pwgen m4 cmake tig xclip gettext neovim libclang-dev fakeroot libncurses-dev iftop git-email neomutt golang-go locales-all pass dstat
 
 $HOME/public-tools/setup-without-root.sh
